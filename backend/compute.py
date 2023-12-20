ad7ed29cdfdf4f8dbb76068e5205820e55aad9ff
mohith2017 <mohith@gobioverse.com>
2023-12-20 10:59:23 -0500

    Added parallel computing functionality

--- backend/compute.py
+++ backend/compute.py
@@ -3,46 +3,53 @@ import os
 import openai
 import json
 import inspect
+import multiprocessing as mp
+
 
 openai.organization = "org-PDgZdabgwEQG4CKvJHhH4ALa"
-openai.api_key = "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"  
+openai.api_key = "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"  
 model_name = "gpt-3.5-turbo"
 
 app = Celery('tasks', broker='pyamqp://guest@localhost//')
 
 @app.task
-def compute_metrics(pipeline_code, metrics_code, inputs):
+def compute_metrics_for_input(args):
     results = []
+    pipeline_code, metrics_code, input = args
     
-    inputs_json = json.dumps(inputs)
-    inputs_dict = json.loads(inputs_json)
-    print("Input: ", type(inputs_dict))    
-
+    print("Input: ", input)    
     print(pipeline_code)
     print(metrics_code)
 
-    for input in inputs_dict:
-        # Create a local scope for the pipeline code
-        pipeline_locals = {}
-        exec(pipeline_code, pipeline_locals)
-        pipeline = pipeline_locals['pipeline']
-        pipeline_output = pipeline(None, input)
 
-        # Execute the metrics code
-        metrics_locals = {}
-        exec(metrics_code, metrics_locals)
-        # Get the metrics functions from the local scope
-        metrics_functions = [v for v in metrics_locals.values() if inspect.isfunction(v)]
+    pipeline_locals = {}
+    exec(pipeline_code, pipeline_locals)
+    pipeline = pipeline_locals['pipeline']
+    pipeline_output = pipeline(None, input)
 
-        for metrics_function in metrics_functions:
-            metrics_output = metrics_function(pipeline_output)
-            # Save the results
-            results.append({
-                'input': input,
-                'pipeline_output': pipeline_output,
-                'metrics_output': metrics_output
-            })
+    metrics_locals = {}
+    exec(metrics_code, metrics_locals)
+    metrics_functions = [v for v in metrics_locals.values() if inspect.isfunction(v)]
+
+    for metrics_function in metrics_functions:
+        metrics_output = metrics_function(pipeline_output)
+        results.append({
+            'input': input,
+            'pipeline_output': pipeline_output,
+            'metrics_output': metrics_output
+        })
     return results
+
+def compute_metrics(pipeline_code, metrics_code, inputs):
+   inputs_json = json.dumps(inputs)
+   inputs_dict = json.loads(inputs_json)
+
+   pool = mp.Pool(mp.cpu_count())
+   results = pool.map(compute_metrics_for_input, [(pipeline_code, metrics_code, input) for input in inputs_dict])
+   pool.close()
+
+   results = [result for sublist in results for result in sublist]
+   return results
 
 
 # print(compute_metrics(pipeline_code, metrics_code, inputs))
